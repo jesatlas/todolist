eee373ba55237b17bee0d2a342039ccac17fc3f6
jesatlas <knoblauchjesse@gmail.com>
2018-12-28 09:12:08 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Contact from './components/Contact';
+import Header from './components/Header';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header branding="contact manager" />
+        <Contact
+          email="karen@gmail.com"
+          name="Karen Smith"
+          phone="555-555-5555"
+        />
       </div>
     );
   }
